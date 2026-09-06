0cc12ecaa342b98fdbeec44afe7d5d9f1670614a
Suvi Tiwary <suvikumari345@gmail.com>
2026-09-06 19:11:43 +0530

    product

--- Kadanes-Algo-for-negative-SW/subarray-product.cpp
+++ Kadanes-Algo-for-negative-SW/subarray-product.cpp
@@ -3,15 +3,23 @@ using namespace std;
 
 int main(){
    
-    int arr[]={-2,4,6,7};
+    int arr[]={-2,4,-6,7};
     int n= sizeof(arr)/sizeof(arr[0]);
 
+    int current_max= arr[0];
+    int current_min= arr[0];
     int current= arr[0];
     int res = 1;
 
     for(int i=1;i<n;i++){
-         
+         current_max = max(current_max*arr[i],arr[i]);
+         current_min = max(current_min*arr[i],arr[i]);
+
+         current = max(current_max,current_min);
+         res = max(res,current);
     }
+
+    cout << "ans : " << res << endl;
 
     return 0;
 }
